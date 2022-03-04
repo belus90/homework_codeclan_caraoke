e608a73afdc41f1251c268d42bd749499ca7bbfd
belus90 <belus90@gmail.com>
2022-03-04 14:03:44 +0000

    Weeken_hw

---
+++ tests/guest_test.py
@@ -0,0 +1,16 @@
+import unittest
+from classes.guest import Guest
+
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.new_guest = Guest("Antal Richard", 24, 15.6)
+
+    def test_guest_has_a_name(self):
+        self.assertEqual("Anfal Richard", self.new_guest.name)
+    
+    def test_guest_has_some_money(self):
+        self.assertEqual(15, self.new_guest.wallet)
+    
+    def test_guest_haas_an_age(self):
+        self.assertEqual(34, self.new_guest.age)
+    
