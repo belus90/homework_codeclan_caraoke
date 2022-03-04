f27775eb13931f25064a03e92be0356d6da8a875
belus90 <belus90@gmail.com>
2022-03-04 14:05:24 +0000

    Guest test

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -6,11 +6,11 @@ class TestGuest(unittest.TestCase):
         self.new_guest = Guest("Antal Richard", 24, 15.6)
 
     def test_guest_has_a_name(self):
-        self.assertEqual("Anfal Richard", self.new_guest.name)
+        self.assertEqual("Antal Richard", self.new_guest.name)
     
     def test_guest_has_some_money(self):
-        self.assertEqual(15, self.new_guest.wallet)
+        self.assertEqual(15.6, self.new_guest.wallet)
     
     def test_guest_haas_an_age(self):
-        self.assertEqual(34, self.new_guest.age)
+        self.assertEqual(24, self.new_guest.age)
     
