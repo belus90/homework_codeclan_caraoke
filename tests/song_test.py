77e010dd8e1c4357ee97d8f0d4987c6fdc359a66
belus90 <belus90@gmail.com>
2022-03-04 15:32:51 +0000

    song test

--- tests/song_test.py
+++ tests/song_test.py
@@ -6,12 +6,11 @@ class TestSong(unittest.TestCase):
         self.song = Song("Mr Bright Side", "American Rock", 2003)
 
     def test_song_name(self):
-        self.assertEqual("Mr Bright", self.song.name)
+        self.assertEqual("Mr Bright Side", self.song.name)
     
     def test_song_type(self):
-        self.assertEqual("Rock", self.song.type)
+        self.assertEqual("American Rock", self.song.type)
     
     def test_maden_year(self):
-        self.assertEqual(1980, self.song.year)
-    
+        self.assertEqual(2003, self.song.year)
     
