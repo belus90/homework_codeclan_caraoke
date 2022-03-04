3f99555b7c76499051548328476ee33de3fe657e
belus90 <belus90@gmail.com>
2022-03-04 14:28:07 +0000

    Room test

--- classes/room.py
+++ classes/room.py
@@ -1,2 +1,9 @@
 class Room:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.guest_room = []
+    
+
+    # def add_guest_to_the_room(self, new_guest):
+    #     self.guest_room.append(new_guest)
+        
--- tests/room_test.py
+++ tests/room_test.py
@@ -2,4 +2,11 @@ import unittest
 from classes.room import Room
 
 class TestRoom(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.room_example = Room("90's room")
+
+    def test_room_has_a_name(self):
+        self.assertEqual("70's song", self.room_example.name)
+    
+    # def test_room_is_empty(self):
+    #     self.assertEqual(0, len())
